3149a9e0f1a39d165dce4b49eb9d33bd5d2e7d27
Christie <stephenson.mchristine@gmail.com>
2014-05-19 10:34:21 -0600

    Fixing database settings

--- settings/test.py
+++ settings/test.py
@@ -5,7 +5,7 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
 # This should be set to the test domain (e.g. test.django-angular-pt.com)
-ALLOWED_HOSTS = ['recipe-backend.christinestephenson.me']
+ALLOWED_HOSTS = ['recipe-backend.christinestephenson.me/']
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '4444444444444444444'
